d778d78fbfb87766d6508a74d2132d8ec82bc932
Josh Long <joshua.long@broadcom.com>
2026-07-03 18:36:05 +0900

    automatic save @ 2026-07-03T09:36:05.229533Z

---
+++ snippets/uao.java
@@ -0,0 +1,16 @@
+// java ~/josh-env/bin/uao.java $HOME/Downloads/demo.zip
+import java.io.*;
+import java.util.function.*;
+import java.util.* ;
+void main(String [] args) throws Exception {
+    var zipFile = new File(args[0]);
+    var zipFileAbsolutePath = zipFile.getAbsolutePath();
+    var folder = new File(zipFileAbsolutePath.substring(0, zipFileAbsolutePath.lastIndexOf(".")));
+    new ProcessBuilder().command("unzip", "-a", zipFileAbsolutePath).inheritIO().start().waitFor();
+    for (var k : Set.of("build.gradle", "build.gradle.kts", "pom.xml")) {
+        var buildFile = new File(folder, k);
+        if (buildFile.exists()) {
+            new ProcessBuilder().command("idea", buildFile.getAbsolutePath()).inheritIO().start().waitFor();
+        }
+    }
+}
